4228964e3e99e072093c5f2dc9b9515e519aefcf
rdzantoine.pro@gmail.com <rdzantoine.pro@gmail.com>
2024-10-26 18:44:35 +0200

    feat: add userviewcreation and token views to urls.py

--- backend/urls.py
+++ backend/urls.py
@@ -1,9 +1,19 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 from core.api.views import CreateUserView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
+    path('api/user/register/', CreateUserView.as_view(), name="register "),
+    # rest_framework_simplejwt views
+    path('api/token/', TokenObtainPairView.as_view(), name='get_token'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    # rest_framework views
+    path('api-auth/', include("rest_framework.urls"))
 ]
