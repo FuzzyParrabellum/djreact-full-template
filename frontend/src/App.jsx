6a0335c1ce1d4e3cee4cea953a5b499a2e79c217
rdzantoine.pro@gmail.com <rdzantoine.pro@gmail.com>
2024-10-27 21:50:37 +0100

    fix: remove bug inducing typos in imports

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
-import react from react
-import {BrowserRouter, Routes, Route, Navigation, Navigate} from "react-router-dom";
+import react from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import Login from "./pages/Login";
 import Home from "./pages/Home";
@@ -8,24 +8,22 @@ import NotFound from "./pages/NotFound";
 import ProtectedRoute from "./components/ProtectedRoute";
 
 function Logout() {
-  localStorage.clear()
-  return <Navigate to='/login/' />
+  localStorage.clear();
+  return <Navigate to="/login/" />;
 }
 
 // we clear the localstorage to remove any tokens before we register, to avoid
 // any token conflict after registering (new tokens will be returned)
 function RegisterAndLogout() {
-  localStorage.clear()
-  return <Register />
+  localStorage.clear();
+  return <Register />;
 }
 
-
 function App() {
-
   return (
     <BrowserRouter>
       <Routes>
-        <Route 
+        <Route
           path="/"
           // Home is for logged in users, so it is protected
           element={
@@ -34,12 +32,12 @@ function App() {
             </ProtectedRoute>
           }
         />
-        <Route path="/login" element={<Login />}/>
-        <Route path="/register" element={<RegisterAndLogout />}/>
-        <Route path="*" element={<NotFound />}/>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<RegisterAndLogout />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
